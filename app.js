c32676c03062c0c4b70d4575851bc78d0a14fbe2
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-01-28 23:57:34 -0300

    Finalizando o Módulo 01-Iniciando com JavaScript

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+
+if (chute == numeroSecreto){
+    alert("Isso ai! você descobriu o número secreto (29) ")
+}
