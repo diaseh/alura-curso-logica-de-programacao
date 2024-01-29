ff8ebd45a7d5fc9bc2150f255d87418ef56c4112
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-01-29 01:44:34 -0300

    Resolvendo o Exercicio 06-Loop infinito

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-alert('Boas-vindas ao jogo do número secreto');
-let nome = prompt("Qual seu Nome");
-let numeroSecreto = 29;
-let chute;
-let tentativas = 1;
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
 
-while(chute != numeroSecreto){
-    chute  = prompt('Escolha um número entre 1 e 30');
-    if (chute == numeroSecreto){
-        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentatia(s)`)
-    } else {
-        if (chute > numeroSecreto){
-            alert(`Você errou o número secreto é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`)
-        }else {
-            alert(`Você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`)
-        }
-        tentativas++;
-    }
-
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+    contador --;
 }
+
+let media = soma / qtdNumeros;
+
+console.log(media);
+
+
+
+
+
+
+
+// alert('Boas-vindas ao jogo do número secreto');
+// let nome = prompt("Qual seu Nome");
+// let numeroSecreto = 29;
+// let chute;
+// let tentativas = 1;
+//
+// while(chute != numeroSecreto){
+//     chute  = prompt('Escolha um número entre 1 e 30');
+//     if (chute == numeroSecreto){
+//         alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentatia(s)`)
+//     } else {
+//         if (chute > numeroSecreto){
+//             alert(`Você errou o número secreto é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`)
+//         }else {
+//             alert(`Você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`)
+//         }
+//         tentativas++;
+//     }
+//
+// }
 
 
 
