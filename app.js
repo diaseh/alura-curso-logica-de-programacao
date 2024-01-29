c30bc05d1995499dda115e540234f0e801522c00
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-01-29 00:08:40 -0300

    Exercicio do 06-Detran

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-alert('Boas-vindas ao jogo do número secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let idade = prompt("Digite sua idade:");
 
-
-if (chute == numeroSecreto){
-    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`)
+if (idade >= 18) {
+    alert("Você é maior de idade.");
 } else {
-    alert(`Você errou`)
+    alert("Você é menor de idade.");
 }
+
+
+
+//
+// alert('Boas-vindas ao jogo do número secreto');
+// let numeroSecreto = 29;
+// let chute = prompt('Escolha um número entre 1 e 30');
+//
+//
+// if (chute == numeroSecreto){
+//     alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`)
+// } else {
+//     alert(`Você errou`)
+// }
