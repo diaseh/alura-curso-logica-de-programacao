5f4828325d3dcef30285c1045eae4e8be97cebca
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-01-29 00:23:29 -0300

    Resolvendo Exercicio 1

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-let idade = prompt("Digite sua idade:");
+// Desafios
+// Pergunte ao usuário qual é o dia da semana.
+//
+//
+//     Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+//
+//     Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!".
+//     Caso contrário, mostre "Tente novamente para ganhar.".
+//     Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+//     Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
 
-if (idade >= 18) {
-    alert("Você é maior de idade.");
-} else {
-    alert("Você é menor de idade.");
+// 1) Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+let dia = prompt("Olá qual é o dia da Semana?")
+    if(dia === "Sabado"){
+        alert("Bom final de Semana")
+    } if(dia === "Domingo"){
+    alert("Bom final de Semana")
+} else{
+        alert("Boa Semana")
 }
+
+
 
 
 
