8a2e89b67b522c5e8588ca0d1ff8befbe5c24a44
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-02-04 13:25:34 -0300

    Exercicio 2 do 07-Desafio: hora da prática

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Desafios
-// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
 
-let contador = 1;
-while (contador <= 10){
+let contador = 10;
+while (contador >= 0){
     alert(contador);
-    contador++;
+    contador--;
 }
 
 
