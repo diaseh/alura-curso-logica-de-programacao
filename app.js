5368a9ae7f5ccf60e2c2d02c93c0853919766d92
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-02-06 02:04:34 -0300

    07-Operador Ternário

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 
-alert('Boas-vindas ao jogo do número secreto');
-let nome = prompt("Qual seu Nome");
-let numeroSecreto = parseInt(Math.random()*100+1);
-let chute;
-let tentativas = 1;
+let palavraPessoa = quantidadePessoas == 1 ? "pessoa" : "pessoas";
 
-while(chute != numeroSecreto){
-    chute  = prompt('Escolha um número entre 1 e 100');
-    if (chute == numeroSecreto){
-        break;
-    } else {
-        if (chute > numeroSecreto){
-            alert(`${nome } você errou o número secreto, o número é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`);
-        }else {
-            alert(`${nome } você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`);
-        }
-        tentativas++;
-    }
 
-}
-let palavraTentativa = tentativas >1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}`);
+
+
+
+// alert('Boas-vindas ao jogo do número secreto');
+// let nome = prompt("Qual seu Nome");
+// let numeroSecreto = parseInt(Math.random()*100+1);
+// let chute;
+// let tentativas = 1;
+//
+// while(chute != numeroSecreto){
+//     chute  = prompt('Escolha um número entre 1 e 100');
+//     if (chute == numeroSecreto){
+//         break;
+//     } else {
+//         if (chute > numeroSecreto){
+//             alert(`${nome } você errou o número secreto, o número é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`);
+//         }else {
+//             alert(`${nome } você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`);
+//         }
+//         tentativas++;
+//     }
+//
+// }
+// let palavraTentativa = tentativas >1 ? 'tentativas' : 'tentativa';
+// alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}`);
 
     // if (tentativas > 1) {
     //     alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas`);
