3e4d165114fb6d01c51f51f022d8b3e90d0024d4
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-02-06 00:03:39 -0300

    02-Break

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
-// Desafios
-// 4)-Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
 
-let numero = prompt("Me passe um número para contar");
-contador =0;
+alert('Boas-vindas ao jogo do número secreto');
+let nome = prompt("Qual seu Nome");
+let numeroSecreto = 29;
+let chute;
+let tentativas = 1;
 
-while(contador <= numero){
-    alert(contador);
-    console.log(contador);
-    contador++;
+while(chute != numeroSecreto){
+    chute  = prompt('Escolha um número entre 1 e 30');
+    if (chute == numeroSecreto){
+        break;
+    } else {
+        if (chute > numeroSecreto){
+            alert(`${nome } você errou o número secreto, o número é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`);
+        }else {
+            alert(`${nome } você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`);
+        }
+        tentativas++;
+    }
 
 }
+    if (tentativas > 1) {
+        alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas`);
+    } else {
+        alert(`Isso ai${nome} você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativa`);
+    }
 
-
-// let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
-// let soma = 0;
-// let contador = qtdNumeros;
-//
-// while(contador > 0){
-//     let numero = parseInt(prompt('Digite o numero:'));
-//     soma += numero;
-//     contador --;
-// }
-//
-// let media = soma / qtdNumeros;
-//
-// console.log(media);
-
-
-
-
-
-
-
-// alert('Boas-vindas ao jogo do número secreto');
-// let nome = prompt("Qual seu Nome");
-// let numeroSecreto = 29;
-// let chute;
-// let tentativas = 1;
-//
-// while(chute != numeroSecreto){
-//     chute  = prompt('Escolha um número entre 1 e 30');
-//     if (chute == numeroSecreto){
-//         alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentatia(s)`)
-//     } else {
-//         if (chute > numeroSecreto){
-//             alert(`Você errou o número secreto é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`)
-//         }else {
-//             alert(`Você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`)
-//         }
-//         tentativas++;
-//     }
-//
-// }
 
 
 
