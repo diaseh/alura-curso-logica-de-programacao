38702295b868d45ee8374c80c93b105d93860412
Eduardo Henrique Dias <eduardo.dias@nucleoe.com.br>
2024-02-12 16:55:19 -0300

    Finalizando o Curso

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-//08 Desafio: hora da prática
-// Desafios finais
-//13)Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+alert('Boas-vindas ao jogo do número secreto');
+let nome = prompt("Qual seu Nome");
+let numeroMaximo = 4000
+let numeroSecreto = parseInt(Math.random()* numeroMaximo+1);
+let chute;
+let tentativas = 1;
 
-let numero = parseInt(Math.random()*1000);
-    alert(`O número gerado automaticamente de 1 a 1000 será ${numero}`)
+while(chute != numeroSecreto){
+    chute  = prompt(`Escolha um número entre 1 a ${numeroMaximo}`);
+    if (chute == numeroSecreto){
+        break;
+    } else {
+        if (chute > numeroSecreto){
+            alert(`${nome } você errou o número secreto, o número é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`);
+        }else {
+            alert(`${nome } você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`);
+        }
+        tentativas++;
+    }
 
-
-
-// alert('Boas-vindas ao jogo do número secreto');
-// let nome = prompt("Qual seu Nome");
-// let numeroSecreto = parseInt(Math.random()*100+1);
-// let chute;
-// let tentativas = 1;
-//
-// while(chute != numeroSecreto){
-//     chute  = prompt('Escolha um número entre 1 e 100');
-//     if (chute == numeroSecreto){
-//         break;
-//     } else {
-//         if (chute > numeroSecreto){
-//             alert(`${nome } você errou o número secreto, o número é menor que ${ chute} esta é sua ${tentativas} tentativa(s)`);
-//         }else {
-//             alert(`${nome } você errou o número secreto é maior que ${ chute} esta é sua ${tentativas} tentativa(s)`);
-//         }
-//         tentativas++;
-//     }
-//
-// }
-// let palavraTentativa = tentativas >1 ? 'tentativas' : 'tentativa';
-// alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}`);
+}
+let palavraTentativa = tentativas >1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}`);
 
     // if (tentativas > 1) {
     //     alert(`Isso ai ${nome}! você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas`);
